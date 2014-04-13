9e75915c059d5f76db2307fe8b3177f6b7c76702
junji.terada <junji.terada@mixi.co.jp>
2014-04-14 00:34:56 +0900

    robolectric is ready to use

---
+++ app/src/test/java/com/example/myapplication/test/MainActivityTest.java
@@ -0,0 +1,14 @@
+package com.example.myapplication.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(RobolectricGradleTestRunner.class)
+public class MainActivityTest {
+    @Test
+    public void shouldPass() throws Exception {
+        assertTrue(true);
+    }
+}
